f88accfb195796cec691aa31d5e589d46a1302a2
sanshamilova <san.shamilova@gmail.com>
2026-04-05 15:05:12 +0300

    Добавлены комментарии к заданию 7

--- task_07_price_dict.py
+++ task_07_price_dict.py
@@ -1,3 +1,4 @@
+#Создание словаря материалов с ценами
 material_prices = {
     "Бетон": 123,
     "Дерево": 456,
@@ -5,21 +6,28 @@ material_prices = {
     "Стекло": 987,
     "Цемент": 654
 }
-print("Список материалов: ")
+#Выведение списка материалов
+print("Список материалов с ценами: ")
 print(material_prices)
 print()
 
+#Добавление 2 новых материалов
 material_prices["Песок"] = 321
 material_prices["Пластик"] = 123
+#Изменение цены одного материала
 material_prices["Бетон"] = 369
+#Удаление одного материала
 del material_prices["Стекло"]
+#Выведение списка материалов
 print("Список материалов после изменений: ")
 print(material_prices)
 print()
 
+#Расчёт средней стоимости материалов
 total_cost = sum(material_prices.values())
 length = len(material_prices)
 
 average_cost = total_cost / length
 
+#Вывод средней стоимости материалов
 print(f"Средняя стоимость материалов: {average_cost:.2f}")

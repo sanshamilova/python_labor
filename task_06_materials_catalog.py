b687d3cc2e73efc611fdf21cd10433c7aa318a92
sanshamilova <san.shamilova@gmail.com>
2026-04-05 14:51:36 +0300

    Внесены изменения в код, добавлены комментарии к заданию 6

--- task_06_materials_catalog.py
+++ task_06_materials_catalog.py
@@ -1,16 +1,21 @@
+#Введение списка материалов
 materials = ["Бетон", "Дерево", "Металл", "Стекло", "Цемент"]
 
+#Выведение первого, среднего и последнего элементов списка
 print("Первый, средний и последний материалы из списка: ")
 print(materials[0])
 print(materials[2])
 print(materials[-1])
 print()
 
+#Добавление новых материалов
 materials.append("Песок")
 materials.append("Пластик")
 
-materials.remove("Пластик")
+#Удаление материала
+materials.remove(materials[1])
 
+#Выведение итогого списка и длины
 print("Итоговый список материалов: ")
 print(materials)
 print()

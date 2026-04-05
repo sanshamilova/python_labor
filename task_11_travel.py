24df2e71aa82b77a2d0e45de0f3f443ca9231bb5
sanshamilova <san.shamilova@gmail.com>
2026-04-05 17:41:01 +0300

    Добавлена часть кода с построением диаграммы к заданию 11

--- task_11_travel.py
+++ task_11_travel.py
@@ -11,3 +11,22 @@ hourly_counts = weekend_data.groupby('hour')['count'].sum().reset_index()
 max_hour = hourly_counts.loc[hourly_counts['count'].idxmax()]
 print(f"Час суток с максимальным числом поездок в выходные: {max_hour['hour']}:00")
 print(f"Количество поездок: {max_hour['count']}")
+
+avg = df_1.groupby('season').agg({
+    'registered': 'mean',
+    'casual': 'mean'
+}).reset_index()
+avg = avg.sort_values('season')
+fig, ax = plt.subplots(figsize=(10, 6))
+
+x = np.arange(len(avg))
+width = 0.35
+
+bars1 = ax.bar(x - width/2, avg['registered'], width,
+               label='Зарегистрированные', color='green', edgecolor='black')
+bars2 = ax.bar(x + width/2, avg['casual'], width,
+               label='Незарегистрированные', color='red', edgecolor='black')
+plt.title("Зависимость числа поездок от сезона")
+plt.xlabel("Время года")
+plt.ylabel("Число поездок")
+plt.show()

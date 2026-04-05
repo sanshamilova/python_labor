52d0e2052190d20726b0783872fd66ce258e1a48
sanshamilova <san.shamilova@gmail.com>
2026-04-05 17:06:55 +0300

    Добавлена часть кода с выводом самого дорогого материала и выводом критических остатков к заданию 10

--- task_10_warehouse_system.py
+++ task_10_warehouse_system.py
@@ -31,3 +31,23 @@ for material, info in warehouse.items():
         print(f"{material:10} | {info['quantity']:>6} | "
           f"{info['price']:>10.2f} | {info['min_quantity']:>5} | {total_value:>10.2f}")
   
+for material, info in warehouse.items():
+    if most_expensive_material_cost / info["price"] == info["quantity"]:
+        most_expensive_material = material
+    else:
+        most_expensive_material = most_expensive_material
+
+print("=" * 75)
+print("Общая стоимость материалов на складе: ", total_cost)
+print("=" * 75)
+print("Самый дорогой материал на складе:", most_expensive_material, {most_expensive_material_cost})
+print("=" * 75)
+print("Критические остатки материала: ")
+for material, info in warehouse.items():
+    if info["quantity"] < info["min_quantity"]:
+        materials_in_danger += 1
+        print("-", material,":", info["quantity"], "<", info["min_quantity"])
+    else:
+        materials_in_danger = materials_in_danger
+print("Критических остатков всего: ", materials_in_danger)
+print("=" * 75)

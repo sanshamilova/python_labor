1cc34af5db79496922c1e866f80d2d980c7f70d3
sanshamilova <san.shamilova@gmail.com>
2026-04-05 14:30:52 +0300

    Добавлены комментарии к заданию 5

--- task_05_materials_vat.py
+++ task_05_materials_vat.py
@@ -1,8 +1,11 @@
+#Введение исходных данных
 price = float(input("Впишите цену за единицу товара: "))
 quantity = int(input("Впишите количество товара: "))
 
+#Расчёт стоимости без скидки
 all_cost = price * quantity
 
+#Система скидок
 if all_cost < 1000:
     sale_coef = 1
 
@@ -12,9 +15,10 @@ elif 1000 <= all_cost <= 5000:
 else:
     sale_coef = 0.9
 
+#Расчёт стоимости с учётом скидки
 end_cost = all_cost * sale_coef
 
-print ("Производим расчет итоговой стоимости: ")
+print ("Производим расчёт итоговой стоимости: ")
 print (f"Стоимость товара без скидки = {price} * {quantity} = {all_cost:.2f}")
-print (f"Стоимость товара с учетом скидки = {all_cost:.2f} * {sale_coef} = {end_cost:.2f} ")
+print (f"Стоимость товара с учётом скидки = {all_cost:.2f} * {sale_coef} = {end_cost:.2f} ")
 print (f"Итоговая стоимость = {end_cost}") 

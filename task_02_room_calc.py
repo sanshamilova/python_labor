f5af9692d68de6539e9221e25fe1b3d6a9760781
sanshamilova <san.shamilova@gmail.com>
2026-04-05 13:43:41 +0300

    Добавлены комментарии к заданию 2

--- task_02_room_calc.py
+++ task_02_room_calc.py
@@ -1,15 +1,20 @@
+#Введение размеров
 length = 6
 width = 3
 height = 2.7
 
+#Введение стоимости покраски 1 м2
 paint_price = 125
 
-floor_area = length * width 
-walls_area = 2 * height * ( length + width )
-room_volume = length * width * height 
+#Расчет геометрических параметров помещения
+floor_area = length * width #Площадь пола
+walls_area = 2 * height * ( length + width ) #Площадь стен
+room_volume = length * width * height #Объём
 
+#Расчет стоимости покраски стен
 painting_cost = paint_price * walls_area
 
+#Вывод результатов расчёта
 print(f"Площадь пола = {floor_area:.2f}")
 print(f"Площадь стен = {walls_area:.2f}")
 print(f"Объем комнаты = {room_volume:.2f}")

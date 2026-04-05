0299213b468429e1b66fe650e99b97ea90a9d347
sanshamilova <san.shamilova@gmail.com>
2026-04-05 15:44:19 +0300

    Добавлены комментарии к заданию 9

--- task_09_address_clean.py
+++ task_09_address_clean.py
@@ -1,27 +1,36 @@
+#Введение исходных адресов
 addresses = [ 
 "  г. Москва, ул. Ленина, д. 10  ", 
 "г.Казань,ул.Баумана,д.15", 
 "  г. Санкт-Петербург, ул. Невский, д. 100  " 
 ]
 
+#Создание нового пустого списка
 clear_adresses = []
 
+#Цикл проверки адресов
 for address in addresses:
-
+   
+    #Удаление лишних пробелов
     address = " ".join(address.split())
     
+    #Добавление пробелов после сокращений
     address = address.replace("г.", "г. ")
     address = address.replace("ул.", "ул. ")
     address = address.replace("д.", "д. ")
     
+    #Унифицирование запятых
     address = address.replace(" ,", ",")
     address = address.replace(", ", ",")
     address = address.replace(",", ", ")
     
+    #Повторное удаление пробелов
     address = " ".join(address.split())
 
+    #Добавление всех элементов после изменений в пустой список
     clear_adresses.append(address)
 
+#Вывод результата
 print("=== СРАВНЕНИЕ ===")
 print()
 print("№1")
